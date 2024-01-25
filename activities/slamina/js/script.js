eafc04a429f68af433fcf6f058bd1e66aaee9379
alyssa32 <alyssa.durdey@gmail.com>
2024-01-25 15:54:49 -0500

    A1: Copied the array of animals

--- activities/slamina/js/script.js
+++ activities/slamina/js/script.js
@@ -8,8 +8,145 @@
 
 "use strict";
 
-let recognizer = new p5.SpeechRec();
-
+const speechRecognizer = new p5.SpeechRec();
+let currentSpeech = `?`;
+let currentAnimal = ``;
+const animals = [
+  "aardvark",
+  "alligator",
+  "alpaca",
+  "antelope",
+  "ape",
+  "armadillo",
+  "baboon",
+  "badger",
+  "bat",
+  "bear",
+  "beaver",
+  "bison",
+  "boar",
+  "buffalo",
+  "bull",
+  "camel",
+  "canary",
+  "capybara",
+  "cat",
+  "chameleon",
+  "cheetah",
+  "chimpanzee",
+  "chinchilla",
+  "chipmunk",
+  "cougar",
+  "cow",
+  "coyote",
+  "crocodile",
+  "crow",
+  "deer",
+  "dingo",
+  "dog",
+  "donkey",
+  "dromedary",
+  "elephant",
+  "elk",
+  "ewe",
+  "ferret",
+  "finch",
+  "fish",
+  "fox",
+  "frog",
+  "gazelle",
+  "gila monster",
+  "giraffe",
+  "gnu",
+  "goat",
+  "gopher",
+  "gorilla",
+  "grizzly bear",
+  "ground hog",
+  "guinea pig",
+  "hamster",
+  "hedgehog",
+  "hippopotamus",
+  "hog",
+  "horse",
+  "hyena",
+  "ibex",
+  "iguana",
+  "impala",
+  "jackal",
+  "jaguar",
+  "kangaroo",
+  "koala",
+  "lamb",
+  "lemur",
+  "leopard",
+  "lion",
+  "lizard",
+  "llama",
+  "lynx",
+  "mandrill",
+  "marmoset",
+  "mink",
+  "mole",
+  "mongoose",
+  "monkey",
+  "moose",
+  "mountain goat",
+  "mouse",
+  "mule",
+  "muskrat",
+  "mustang",
+  "mynah bird",
+  "newt",
+  "ocelot",
+  "opossum",
+  "orangutan",
+  "oryx",
+  "otter",
+  "ox",
+  "panda",
+  "panther",
+  "parakeet",
+  "parrot",
+  "pig",
+  "platypus",
+  "polar bear",
+  "porcupine",
+  "porpoise",
+  "prairie dog",
+  "puma",
+  "rabbit",
+  "raccoon",
+  "ram",
+  "rat",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "salamander",
+  "seal",
+  "sheep",
+  "shrew",
+  "silver fox",
+  "skunk",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tapir",
+  "tiger",
+  "toad",
+  "turtle",
+  "walrus",
+  "warthog",
+  "weasel",
+  "whale",
+  "wildcat",
+  "wolf",
+  "wolverine",
+  "wombat",
+  "woodchuck",
+  "yak",
+  "zebra",
+];
 let bg = {
   r: 158,
   g: 219,
@@ -25,8 +162,8 @@ function preload() {}
  * Description of setup
  */
 function setup() {
-  recognizer.onResult = handleResult;
-  recognizer.start();
+  speechRecognizer.onResult = handleSpeechInput;
+  speechRecognizer.start();
 
   createCanvas(800, 800);
 }
@@ -35,10 +172,12 @@ function setup() {
  */
 function draw() {
   background(bg.r, bg.g, bg.b);
+  textSize(32);
+  text(currentSpeech, width / 2, height / 2);
 }
 
 function mousePressed() {}
 
-function handleResult() {
-  console.log(recognizer.resultString);
+function handleSpeechInput() {
+  currentSpeech = speechRecognizer.resultString;
 }
