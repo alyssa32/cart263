aac1afd13792ab6592b94cdd35171a02db9680ad
alyssa32 <alyssa.durdey@gmail.com>
2024-02-05 12:48:44 -0500

    J1: Created a speechRecognizer and a Black Canvas

--- jams/voice-jam/js/script.js
+++ jams/voice-jam/js/script.js
@@ -1,32 +1,37 @@
 /**
  * Title of Project
  * Author Name
- * 
- * This is a template. You must fill in the title, author, 
+ *
+ * This is a template. You must fill in the title, author,
  * and this description to match your project!
  */
 
 "use strict";
-
+let canvas = {
+  x: 1450,
+  y: 850,
+};
+const speechRecognizer = new p5.SpeechRec();
+let currentSpeech = "?";
 /**
- * Description of preload
-*/
-function preload() {
-
-}
-
+ * Preloads all my used images
+ */
+function preload() {}
 
 /**
  * Description of setup
-*/
+ */
 function setup() {
-
+  createCanvas(canvas.x, canvas.y);
+  speechRecognizer.onResult = handleSpeechInput;
+  speechRecognizer.start();
 }
-
 
 /**
  * Description of draw()
-*/
+ */
 function draw() {
-
+  background(0, 0, 0);
 }
+
+function handleSpeechInput() {}
