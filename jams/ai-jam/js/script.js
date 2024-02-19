3b26850225093c0e50996c55b663105c21808ad7
alyssa32 <alyssa.durdey@gmail.com>
2024-02-19 16:28:44 -0500

    J2: Added the Script Tag and Black Bg

--- jams/ai-jam/js/script.js
+++ jams/ai-jam/js/script.js
@@ -1,32 +1,33 @@
 /**
  * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
+ * Alyssa Durdey
+ *
+ * This is a template. You must fill in the title, author,
  * and this description to match your project!
  */
 
 "use strict";
+//The user's webcam
+let video = undefined;
 
 /**
  * Description of preload
-*/
-function preload() {
-
-}
-
+ */
+function preload() {}
 
 /**
  * Description of setup
-*/
+ */
 function setup() {
-
+  createCanvas(1450, 820);
+  //Access the user's webcam
+  video = createCapture(VIDEO);
+  video.hide();
 }
-
 
 /**
  * Description of draw()
-*/
+ */
 function draw() {
-
+  background(0);
 }
