eb10feaa46b45eb015cc98d79eff716ba2cc1929
alyssa32 <alyssa.durdey@gmail.com>
2024-03-07 16:05:37 -0500

    J3: Created a Play Class File

---
+++ jams/game-engine/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let = configuration = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: `arcade`,
+  },
+  scene: [],
+};
+
+let game = new Phaser.Game(configuration);
