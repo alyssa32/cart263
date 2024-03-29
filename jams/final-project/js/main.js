127a719120b8ee0548d844de969d7f277de381e9
alyssa32 <alyssa.durdey@gmail.com>
2024-03-28 21:37:27 -0400

    FP: Duplicated the Jam

---
+++ jams/final-project/js/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let config = {
+  // The type refers to the kind of display we'll be using
+  type: Phaser.AUTO,
+  width: 1420,
+  height: 792,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 500 },
+      debug: true,
+    },
+  },
+  // The scene property has an array of the different scenes
+  scene: [Boot, Play],
+};
+
+// Creates the game using this configuration!
+let game = new Phaser.Game(config);
