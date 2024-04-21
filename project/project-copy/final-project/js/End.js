dda8def3b7bf7a0207f60b8266dfa536509e8a88
alyssa32 <alyssa.durdey@gmail.com>
2024-04-20 20:55:57 -0400

    FP: Added the Image to the End Screen

--- project/project-copy/final-project/js/End.js
+++ project/project-copy/final-project/js/End.js
@@ -11,7 +11,36 @@ class End extends Phaser.Scene {
   //*
   //Will be called one time (setup() equivalent)
   //*
-  create() {}
+  create() {
+    //Dislays the image of both characters
+    this.add.image(710, 330, "playersGrown").setScale(0.4);
+    //Colours the background black
+    this.cameras.main.setBackgroundColor("#ebebeb");
+    //Displays the "Restart" button
+    const button = this.add
+      .text(730, 600, "Restart", {
+        fontFamily: "Arial",
+        fontSize: "32px",
+        color: "#ffffff",
+        align: "center",
+        fixedWidth: 260,
+        backgroundColor: "#5fb359",
+      })
+      .setPadding(32)
+      .setOrigin(0.5);
+
+    button.setInteractive({ useHandCursor: true });
+
+    button.on("pointerover", () => {
+      button.setBackgroundColor("#b2eb50");
+      button.setColor("#ffffff");
+    });
+
+    button.on("pointerout", () => {
+      button.setBackgroundColor("#5fb359");
+      button.setColor("#000000");
+    });
+  }
   //*
   //Will constantly be called (draw() equivalent)
   //*
